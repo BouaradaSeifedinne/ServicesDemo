7290e4bef4f0da2dba7ec921d48c41c7ae9d6d08
seif.bouarada <seif_bouarada@outlook.com>
2018-04-29 10:48:53 +0100

    seifscauf@gmail.com

--- src/app/home/container/register/register.component.ts
+++ src/app/home/container/register/register.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterService } from '../../../providers/register.service';
 import {TranslateService} from '@ngx-translate/core';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,11 +17,12 @@ export class RegisterComponent implements OnInit {
   email:string;
   password:string;
 
-  constructor(private http:HttpClient, private register:RegisterService, private translate: TranslateService) {
+  constructor(private http:HttpClient, private register:RegisterService, private translate: TranslateService, public toastr: ToastsManager, vcr: ViewContainerRef, private route:Router) {
     translate.addLangs(["en", "fr"]);
     translate.setDefaultLang('en');
     let browserLang = translate.getBrowserLang();
     translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+    this.toastr.setRootViewContainerRef(vcr);
    }
 
   ngOnInit() {
@@ -33,8 +36,18 @@ export class RegisterComponent implements OnInit {
       password:this.password
     }
 
-    this.register.register(data, this.translate.currentLang).subscribe(data => {
-      console.log(data);
+    this.register.register(data, this.translate.currentLang).subscribe(res => {
+      let array = Object.values(res);
+      this.toastr.success(array[0], 'Success!');
+      
+      
+    },
+    error => {
+      console.log(error);
+      if(error.error.err){
+        this.toastr.warning(error.error.err, 'Alert!');
+        this.toastr.error(error.statusText, error.status);
+      }
     });
   }
 
